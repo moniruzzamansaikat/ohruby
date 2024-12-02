cec1be43720873011b1afce5481e1a464eec9273
moniruzzamansaikat <saikat.moniruzzaman@gmail.com>
2024-12-03 00:50:39 +0600

    rackfile

---
+++ config/rack_profiler.rb
@@ -0,0 +1,3 @@
+Rack::MiniProfiler.config.position = 'bottom' # Adjust to 'left' if preferred
+Rack::MiniProfiler.config.start_hidden = true
+Rack::MiniProfiler.config.skip_paths = ['/assets'] # Optional: Ignore asset requests
